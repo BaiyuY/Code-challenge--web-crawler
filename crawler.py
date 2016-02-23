d61828dd309f628985b0a319fcbd5bb3e7fb65f8
Brandy <ybywmr@163.com>
2016-02-22 20:34:26 -0600

    Change the data structure of email_list to stack; Correct typos on naming conventions;

--- crawler.py
+++ crawler.py
@@ -1,107 +1,99 @@
 from Scraper import *
 import sys
 import shutil
-import os 
+import os
 import time
 import threading
-
-
 
 domain = None
 crawler = None
 timeout = 5
-depth = 10
+depth = 3
 lag = 0.1
 bots = ["Googlebot/2.1 (+http://www.google.com/bot.html)", "Mozilla/5.0 (compatible; Yahoo! Slurp; http://help.yahoo.com/help/us/ysearch/slurp)", "Mozilla/5.0 (compatible; bingbot/2.0; +http://www.bing.com/bingbot.htm)"]
-bot = bots[1]
-emailscan = False
 tprint = None
 
 # Initializes everything
 def init():
-	global crawler
-	global domain
-	global bot
-	global timeout
-	global lag
-	global depth
-	global emailscan
-	global tprint
-	
-	# Remove previous files
-	try:
-		shutil.rmtree(".cache")
-	except OSError:
-		pass
-		
-	try:
-		os.remove("emails.txt")
-	except OSError:
-		pass
-	
-	# Process cmd line arguments
-	cmdArgs()
-	
-	# Initialize scraper object
-	crawler = Scraper(domain, bot, timeout, lag, depth, emailscan)
+    global crawler
+    global domain
+    global bot
+    global timeout
+    global lag
+    global depth
+    global tprint
 
-	# Pretty print thread
-	tprint = threading.Thread(target=ThreadPrettyPrint)
-	
+    # Remove previous files
+    try:
+        shutil.rmtree(".cache")
+    except OSError:
+        pass
+
+    try:
+        os.remove("emails.txt")
+    except OSError:
+        pass
+
+    # Process cmd line arguments
+    cmd_args()
+
+    # Initialize scraper object
+    crawler = Scraper(domain, bot, timeout, lag, depth)
+
+    # Pretty print thread
+    tprint = threading.Thread(target=thread_pretty_print)
+
+
 # Starts scraping
 def start():
-	global crawler
-	global tprint
-	
-	tprint.start()
-	crawler.Scrape()
+    global crawler
+    global tprint
+
+    tprint.start()
+    crawler.Scrape()
+
 
 # Called when the application is going to end
 def stop():
-	global crawler
-	# Remove .cache if it exists
-	shutil.rmtree(".cache")
-	crawler.shouldDie = True
+    global crawler
+    # Remove .cache if it exists
+    shutil.rmtree(".cache")
+    crawler.should_die = True
+
 
 # Command line args
-def cmdArgs():
-	global domain
-	global bot
-	global timeout
-	global lag
-	global depth
-	global emailscan
-	
-	
+def cmd_args():
+    global domain
+    global bot
+    global timeout
+    global lag
+    global depth
 
-	# Recover full domain
-	domain = "http://" + sys.argv[1]
+    # Recover full domain
+    domain = "http://" + sys.argv[1]
 
-	# Add "/" to end of domain
-	if not sys.argv[1].endswith("/"):
-		domain = domain + "/"
+    # Add "/" to end of domain
+    if not sys.argv[1].endswith("/"):
+        domain = domain + "/"
 
 
+def thread_pretty_print():
+    global crawler
 
-def ThreadPrettyPrint():
-	global crawler
-	
+    print "User-agent: " + crawler.bot
+    print "Depth: " + str(crawler.depth)
+    print "Lag: " + str(crawler.lag)
+    print "Timeout: " + str(crawler.timeout)
 
-	print "User-agent: " + crawler.bot
-	print "Depth: " + str(crawler.depth)
-	print "Lag: " + str(crawler.lag)
-	print "Timeout: " + str(crawler.timeout)
-	
-	while crawler.shouldDie != True:
-		print "Cycles: " + str(crawler.cycles)
-		print "Remaining Links: " + str(len(crawler.mainList))
-		print "Scanned links: " + str(len(crawler.doneList))
-		
-		if crawler.emailscan == True:
-			print "Found these email addresses: " + str(len(crawler.emailList)) # Only print email list when it is not empty
-		
-		print
-		time.sleep(1)
+    while crawler.should_die != True:
+        print "Cycles: " + str(crawler.cycles)
+        print "Remaining Links: " + str(len(crawler.main_list))
+        print "Scanned links: " + str(len(crawler.done_list))
+
+        print "Found these email addresses: " + str(len(crawler.email_list)) # Only print email list when it is not empty
+
+        print
+        time.sleep(1)
 
 
 init()
